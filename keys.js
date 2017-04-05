f44142814bf61955a86d402a5424bb4de9cc3428
Joanna Barajas <joannabarajas@joannabmacbook2.fios-router.home>
2017-04-05 14:57:18 -0700

    setup for twitter and spotify

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: "IWts7bwBhFNM9qnYtE7ZkhHD1",
+    consumer_secret: "Q61urvGOj4Ifqq8ECBsqAcZJnvCxTVXUFB7hISVQkX1KLlmFug",
+    access_token_key: "233082682-8dssfVg1tEe9HYYOOdIiicHTqubsGZKUSGZJ62lr",
+    access_token_secret: "jFEnv5hg7YjAcjP9HrAWQOCg6aNQRjXjxsiZn40HXPicQ"
+}
